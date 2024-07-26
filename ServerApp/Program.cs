62548eda0eeac29203caa63effe93acd86cfd13d
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-07-26 16:09:56 +0200

    first commit

---
+++ ServerApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, 1!");
+Console.WriteLine("Hello, 2!");
+Console.WriteLine("Hello, 3!");
---
+++ readme.md
@@ -0,0 +1,5 @@
+
+# dotnet for client-server
+
+## ispiration to begin
+<https://learn.microsoft.com/en-us/dotnet/fundamentals/networking/sockets/socket-services>
